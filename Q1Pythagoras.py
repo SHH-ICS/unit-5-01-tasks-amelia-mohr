4cb7fa2f7d80c9315493b6c2e01576f2b29b39a1
Amelia Mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-04-26 13:58:18 +0000

    fix

--- Q1Pythagoras.py
+++ Q1Pythagoras.py
@@ -1,2 +1,23 @@
 # Create a program that will accept the two legs of a right-angle triangle, a and b, and display the length of the hypotenuse, c. 
 # Remember to use prompts for the input and labels for the output. Use the formula a2 + b2 = c2 to calculate your answer.
+
+import math
+import decimal
+decimal.getcontext().rounding = decimal.ROUND_HALF_UP
+finished = False 
+while not finished:
+    try:
+        a = float(input("Please enter a length for leg a of your triangle: "))
+        if a <= 0:
+            raise ValueError
+        b = float(input("Please enter a length for leg b of your triangle: "))
+        if b <= 0:
+            raise ValueError
+        u = str(input("Please enter a unit (ex. cm, ft, etc.): "))
+        firstc = (a**2) + (b**2)
+        secondc = math.sqrt(firstc)
+        c = round(decimal.Decimal(str(secondc)))
+        print("All results are rounded to 2 decimal places.")
+        print("The hypotenuse of your triangle is: " + str(c) + " " + u)
+    except ValueError:
+        print("Please enter a POSITIVE NUMBER!")
