0c653d8ebc60de96c85add22053ecbbbcd7f44e2
Amelia Mohr <124377942+amelia-mohr@users.noreply.github.com>
2023-04-26 14:12:13 +0000

    fix

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,17 @@
 # Create a program that accepts 2 numbers from the user. 
 # Your program will output a random number between the range given by the user.
+
+import random
+finished = False
+while not finished:
+    try:
+        print("Please enter your numbers from lowest to highest.")
+        a = float(input("Please enter your first number: "))
+        b = float(input("Please enter your second number: "))
+        if a > b:
+            a, b = b, a
+        randomnumber = random.uniform(a, b)
+        print("Your random number is: " + str(randomnumber))
+        finished = True
+    except ValueError:
+        print("Please don't enter text!")
